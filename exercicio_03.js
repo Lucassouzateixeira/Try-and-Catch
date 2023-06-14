9261e9ea26c57a664499a631f4fce5d543ab3c14
Lucassouzateixeira <lucassouzateixeira2@gmail.com>
2023-06-14 13:43:02 -0400

    Atividade concluída

---
+++ exercicio_03.js
@@ -0,0 +1,35 @@
+class MeuErro extends Error {
+  constructor(message){
+    super(message);
+    this.name = "Meu Erro";
+  }
+}
+
+
+class NerdIF {
+  constructor(estudante, cosplay, nota_cosplay) {
+    this.estudante = estudante;
+    this.cosplay = cosplay;
+    this.nota_cosplay = nota_cosplay;
+  }
+
+  retornarAtributos() {
+    return this.atributos()
+  }
+
+  atributos() {
+    return {
+      estudante: this.estudante,
+      cosplay: this.cosplay,
+      nota_cosplay: this.nota_cosplay
+    };
+  }
+}
+
+const aluno = new NerdIF("João", "Homem-Aranha", 9.5);
+const atributos = aluno.retornarAtributos();
+
+console.log(atributos.estudante); 
+console.log(atributos.cosplay);   
+console.log(atributos.nota_cosplay);
+
